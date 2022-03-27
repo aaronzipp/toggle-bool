abe00abb7128208784315c906a0391771f4867f1
Aaron Zipp <aaronzipp@protonmail.com>
2022-03-27 23:19:03 +0200

    Added unit tests and GitHub Actions

---
+++ python/tests/test_base.py
@@ -0,0 +1,16 @@
+import toggle_bool
+import pytest # noqa
+from enum import Enum
+
+
+class Case(str, Enum):
+    UPPERCASE = "UPPERCASE"
+    LOWERCASE = "lowercase"
+    TITLE = "Title"
+
+
+def test_format_value():
+    test_string = "TeSt"
+    assert toggle_bool.format_value(Case.UPPERCASE, test_string) == test_string.upper()
+    assert toggle_bool.format_value(Case.LOWERCASE, test_string) == test_string.lower()
+    assert toggle_bool.format_value(Case.TITLE, test_string) == test_string.title()
